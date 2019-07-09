6b3d9f6f3646a19b50d79e79f101e547fb41111d
dhams2020 <44002858+dhamodharan-c@users.noreply.github.com>
2019-07-09 21:22:10 +0530

    Create py8.py

---
+++ py8.py
@@ -0,0 +1,4 @@
+inp=int(input())+1           #get the input value ande increment by '1' because the range function work n-1
+value=sum(list(range(inp))) #take each value and and store in it value
+print(value)  #print the value
+
