243e0e332c01e88df88538f291613a373b1437ab
dhams2020 <44002858+dhamodharan-c@users.noreply.github.com>
2019-07-10 23:40:27 +0530

    Create py14.py

---
+++ py14.py
@@ -0,0 +1,8 @@
+#dispaly odd numbers between two intervals
+inp=list(map(int,input().split()))  #get the input
+final=[]                            #append the final
+for i in range(inp[0]+1,inp[1]):    #iteration
+    if i%2!=0:
+        final.append(str(i))        #append the odd value to the final
+print(" ".join(final))              #print the final 
+
