412575b5421e79f6f5251649ec31aaf36e2d9f84
dhams2020 <44002858+dhamodharan-c@users.noreply.github.com>
2019-07-11 17:19:09 +0530

    Update py19.py

--- py19.py
+++ py19.py
@@ -1,5 +1,5 @@
 #factorial of the number
 import math     #import math
 inp=int(input())  #get the input
-
-print(math.factorial(inp))  #print the factorail number
+if inp<=20:
+  print(math.factorial(inp))  #print the factorail number
