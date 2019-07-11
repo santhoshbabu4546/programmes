a0c5c49873398bc2fb84525448631537ddb6f97b
dhams2020 <44002858+dhamodharan-c@users.noreply.github.com>
2019-07-11 21:40:28 +0530

    Create p5.py

---
+++ p5.py
@@ -0,0 +1,5 @@
+#identify the equvalent integer for given romen letter. 
+inp=input()
+roman=['I','II','III','IV','V','VI','VII','VII','IX','X','XI','XII','XIII','XIV','XV','XVII','XVIII','XIX','XX'] #declare the list untill 20
+
+print(roman.index(inp)+1)  #print the value
