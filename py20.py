d4a50cf2ee8d5485b3339bb74519a526325c5832
dhams2020 <44002858+dhamodharan-c@users.noreply.github.com>
2019-07-11 17:07:28 +0530

    Update py20.py

--- py20.py
+++ py20.py
@@ -1,6 +1,6 @@
 #given number N find the first multiples of 5
 inp=int(input())  #get the input
 final=[]
-for i in range(1,5): #iterate the given range
+for i in range(1,6): #iterate the given range
     final.append(str(i*inp))  #calculate the multiples of 'N'
 print(" ".join(final))  #finally join the values
