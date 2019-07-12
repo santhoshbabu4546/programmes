97fdd7b9972d074d5431dc74e856f6e8a8dba0f6
dhams2020 <44002858+dhamodharan-c@users.noreply.github.com>
2019-07-12 16:55:31 +0530

    Create p12.py

---
+++ p12.py
@@ -0,0 +1,7 @@
+#rotate the array
+inp=list(map(int,input().split()))  #get the input size and number of times rotate
+values=input().split()             
+values="".join(values)             #convert into string
+for _ in range(inp[1]):
+    values=(str(values[-1])+str(values[:len(values)-1]))   #rotation
+print(values)       #print the values
